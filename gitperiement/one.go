de9461840abca112b9d38e955d7d51660fd4a909
Emily <all@something.local>
2018-04-15 08:14:14 -0700

    for practice

---
+++ gitperiement/one.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func add(danum int)int{
+  doubler :=danum + danum
+  return doubler
+}
+
+func main(){
+fmt.Println("Hello World")
+dacall :=add(2)
+fmt.Println(dacall)
+fmt.Println("commit but then remove this")
+
+}
